0fa439e4c696febb3e0e3c1c910698a6a2663414
Łukasz Cieśnik <lukasz.ciesnik@gmail.com>
2025-12-15 00:51:14 +0100

    building frontend with webpack

--- TODO.md
+++ TODO.md
@@ -1,9 +1,10 @@
 * [ ] Use express? https://docs.netlify.com/build/frameworks/framework-setup-guides/express/
 * [ ] GET response caching (mailing lists etc.)
-* [ ] hcaptcha
+* [ ] hcaptcha?
 * [x] API redirection
 * [x] rename src=>backend dist=>frontend
 * [x] minimize function call - captcha site key file should be generated at build time
 * [ ] json schema for request validation
 * [ ] use https://www.netlify.com/blog/introducing-netlify-functions-2-0/
-* [ ] webpack for frontend
+* [x] webpack for frontend
+* [ ] typescript for frontend
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+// Frontend build
+
+const webpack = require('webpack');
+const path = require('path');
+
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const GenerateJsonPlugin = require('generate-json-webpack-plugin');
+
+// @todo only use RECAPTCHA_SITE_KEY
+// https://webpack.js.org/configuration/dotenv/
+require('dotenv').config();
+
+module.exports = {
+  mode: process.env.NODE_ENV === 'develepoment' ? 'development' : 'production',
+  entry: {
+    main: path.resolve(__dirname, 'frontend/mailer.js'),
+  },
+  output: {
+    path: path.resolve(__dirname, 'frontend/dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Subscribe control panel',
+      template: 'frontend/index.html'
+    }),
+    new HtmlWebpackPlugin({
+      filename: '404.html',
+      template: 'frontend/404.html'
+    }),
+    new GenerateJsonPlugin('api/recaptcha', {
+      success: true,
+      recaptcha_site_key: process.env.RECAPTCHA_SITE_KEY
+    }),
+  ],
+  module: {
+    rules: [
+    ],
+  },
+};
+
